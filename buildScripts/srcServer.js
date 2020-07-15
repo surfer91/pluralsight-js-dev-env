c3768dffd25ff4bb2773b2de189a1222153abfac
Krzysztof Klimczak <l2eu@wp.pl>
2020-07-15 19:53:53 +0200

    Set up Express

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path=require('path');
+const open=require('open');
+const port= 3000;
+const app=express();
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname,'../src/index.html'));
+});
+
+app.listen(port,function(err){
+if (err)
+{console.log(err);}
+else
+{ (async()=>{await open('http://localhost:'+port);})()
+
+
+
+}
+
+})
